5dd3ce5506497c40fc6b6790df84ad4e692d34ef
Edgar Diaz <ed35376@gmail.com>
2023-04-23 18:06:18 -0400

    possibilty to change width and heigth

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ import "./style.css";
 
 let body = document.body;
 body.style.background = "green";
+//Setting atributes of principal DIv
 
 let parentdiv = document.querySelector("div");
 parentdiv.id = "principal";
+
+//Create card elements
 
 let cardBody = document.createElement("div");
 cardBody.id = "card";
@@ -19,7 +22,6 @@ cardBody.style.border = "1px solid";
 cardBody.style.margin = "2% auto";
 cardBody.style.display = "inline-block";
 cardBody.style.position = "relative";
-parentdiv.appendChild(cardBody);
 
 let cardTop = document.createElement("span");
 cardTop.id = "top";
@@ -27,9 +29,7 @@ cardTop.style.top = "20px";
 cardTop.style.left = "20px";
 cardTop.style.fontSize = "70px";
 cardTop.style.position = "absolute";
-
 cardTop.style.color = "black";
-cardBody.appendChild(cardTop);
 
 let cardBottom = document.createElement("span");
 cardBottom.id = "bottom";
@@ -37,8 +37,6 @@ cardBottom.style.bottom = "20px";
 cardBottom.style.right = "20px";
 cardBottom.style.fontSize = "70px";
 cardBottom.style.position = "absolute";
-
-cardBody.appendChild(cardBottom);
 
 let cardNumber = document.createElement("span");
 cardNumber.id = "number";
@@ -48,11 +46,19 @@ cardNumber.style.position = "absolute";
 cardNumber.style.margin = "-25px 0 0 -25px";
 cardNumber.style.fontSize = "60px";
 
+// Append card body to parent div
+parentdiv.appendChild(cardBody);
+// Append card elements to card body
+
+cardBody.appendChild(cardTop);
+cardBody.appendChild(cardBottom);
 cardBody.appendChild(cardNumber);
 
+// Define values and palos arrays
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let palos = ["♦", "♥", "♠", "♣"];
 
+// Generate random card values and palos function
 let generateRandomCard = () => {
   let randomValue = Math.floor(Math.random() * 13);
   cardNumber.innerHTML = values[randomValue];
@@ -70,10 +76,11 @@ let generateRandomCard = () => {
   }
 };
 
+//Call generateRandomCard when the page loads
 window.onload = () => {
   generateRandomCard();
 };
-
+//Extras define a ramdom card Button
 let space = document.createElement("br");
 parentdiv.appendChild(space);
 
@@ -85,4 +92,25 @@ parentdiv.appendChild(button1);
 button1.onclick = function() {
   generateRandomCard();
 };
-// input1.addEventListener("click", alert("hola"));
+
+//Extra define a Width and Heigth changer
+let space2 = document.createElement("br");
+parentdiv.appendChild(space2);
+
+let input1 = document.createElement("input");
+input1.placeholder = "width";
+parentdiv.appendChild(input1);
+
+let input2 = document.createElement("input");
+input2.placeholder = "heigth";
+parentdiv.appendChild(input2);
+
+let button2 = document.createElement("button");
+button2.id = "sizeChanger";
+button2.textContent = "Card Size";
+parentdiv.appendChild(button2);
+
+button2.addEventListener("click", () => {
+  cardBody.style.width = input1.value + "px";
+  cardBody.style.height = input2.value + "px";
+});
